6e0b0523835923dbe0ede679e00ed7d321782ef0
Daniel Im <chorales@naver.com>
2023-05-19 23:55:52 +0900

    Initial Commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import {GraphQLModule} from "@nestjs/graphql";
+import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo";
+import {UserModule} from "@/apis/user/user.module";
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schema.gql',
+    }),
+    UserModule
+  ],
+})
+export class AppModule {}
